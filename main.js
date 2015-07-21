d54b91c82ba0484ee0348041e9a4175ab12a60f8
Jose Garcia <garciajd90@gmail.com>
2015-07-20 23:48:45 -0700

    instantiate a rectangle that can be moved with arrow keys

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
+function clearCanvas(canvasContext) {
+	canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function Rect(canvasContext, x, y, width, height) {
+	var MOVEMENT_INCREMENT = 5;
+
+	this.canvasContext = canvasContext;
+	this.x = x || 0;
+	this.y = y || 0;
+	this.width = width || 50;
+	this.height = height || 50;
+
+	this.draw = function draw() {
+		this.canvasContext.fillRect(this.x, this.y, this.width, this.height);
+	};
+
+	this.keydownHandler = function keydownHandler(event) {
+		clearCanvas(this.canvasContext);
+		console.log(event.keyCode);
+
+		switch (event.keyCode) {
+			case 37: // Left
+				this.x -= MOVEMENT_INCREMENT;
+				break;
+			case 38: // Up
+				this.y -= MOVEMENT_INCREMENT;
+				break;
+			case 39: // Right
+				this.x += MOVEMENT_INCREMENT;
+				break;
+			case 40: // Down
+				this.y += MOVEMENT_INCREMENT;
+		}
+
+		this.draw();
+	};
+}
+
 (function() {
 	var canvas = document.getElementById('canvas');
 	var context = canvas.getContext('2d');
-
 	context.fillStyle = 'rgb(200, 0, 0';
-	context.fillRect(50, 50, 50, 50);
+	
+	var rect = new Rect(context);
+	rect.draw();
+
+	window.addEventListener('keydown', rect.keydownHandler.bind(rect));
 })();
