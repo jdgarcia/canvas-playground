67760cefcef2d811c75cd9225240fc1336628c1c
Jose Garcia <garciajd90@gmail.com>
2015-07-20 23:18:59 -0700

    it draws a box

---
+++ main.js
@@ -0,0 +1,7 @@
+(function() {
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+
+	context.fillStyle = 'rgb(200, 0, 0';
+	context.fillRect(50, 50, 50, 50);
+})();
