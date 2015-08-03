06ba53af701462e01c6940de775052c8debe1613
Jose Garcia <garciajd90@gmail.com>
2015-08-02 17:29:44 -0700

    some sort of pattern type thing

--- js/colors.js
+++ js/colors.js
@@ -2,24 +2,40 @@
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
 
-  var x = 0;
-  var y = 0;
-  var r = 0;
-  var g = 0;
-  var b = 0;
-
-  var PIXEL_SIZE = 10;
-
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max + 1 - min)) + min;
   }
 
-  while (x < canvas.width && y < canvas.height) {
-    r = getRandomInt(0, 255);
-    g = getRandomInt(0, 255);
-    b = getRandomInt(0, 255);
+  function getModifiedValue(value) {
+    var newValue;
 
-    context.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+    if (value === 0) {
+      newValue = 1;
+    } else if (value === 255) {
+      newValue = 254;
+    } else {
+      newValue = Math.random() < 0.5 ? value + 1 : value - 1;
+    }
+
+    return newValue % 256;
+  }
+
+  var x = 0;
+  var y = 0;
+  var r = getRandomInt(0, 255);
+  var g = getRandomInt(0, 255);
+  var b = getRandomInt(0, 255);
+
+  var PIXEL_SIZE = 10;
+
+  while (x < canvas.width && y < canvas.height) {
+    console.log(x, y);
+    r = getModifiedValue(r);
+    g = getModifiedValue(g);
+    b = getModifiedValue(b);
+
+    var color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    context.fillStyle = color;
     context.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
 
     x += PIXEL_SIZE;
